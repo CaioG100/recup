3e68d96efaacb4b914af46b088a50a7e44fbea9f
Caio Gil <caiogil100@gmail.com>
2022-03-04 23:18:09 -0300

    recup

---
+++ recup.php
@@ -0,0 +1,11 @@
+try {
+    $conn = new PDO('mysql:host=localhost;dbname=meuBancoDeDados', $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare('SELECT * FROM minhaTabela WHERE id = :id');
+    $stmt->execute(array('id' => $id));
+    while($row = $stmt->fetch()) {
+        print_r($row);
+    }
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
